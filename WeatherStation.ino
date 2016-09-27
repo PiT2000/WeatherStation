8f147f374ed91c67eb0fc567eb384d2522832689
Drexstor <Drexstor@gmail.com>
2016-09-27 19:50:17 +0400

    Add wind speed, rail setsor is calibrated

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -2,9 +2,9 @@
 #include "Barometer.h"
 #include <Wire.h>
 
-#define A_0_fallout 0
-#define A_2_speed_N_S 2
-#define A_4_speed_W_E 4
+#define A_0_precipitation 0
+#define A_1_speed_N_S 1
+#define A_2_speed_W_E 2
 #define DataPin  10
 #define ClockPin 11
 SHT1x sht1x(DataPin, ClockPin);
@@ -14,10 +14,9 @@ Barometer myBarometer;
 float pressure;
 float temp_c;
 float humidity;
-float fallout;
 float speed_N_S;
 float speed_W_E;
-
+float precipitation;
 long time;
 
 void setup()
@@ -28,31 +27,38 @@ void setup()
 
 void loop()
 {
+  unsigned long speed_N_S = 0;
+  unsigned long speed_W_E = 0;
   pressure = myBarometer.bmp085GetPressure(myBarometer.bmp085ReadUP());
   temp_c = sht1x.readTemperatureC();
   humidity = sht1x.readHumidity();
-
-  fallout = analogRead(A_0_fallout);
-  speed_N_S = analogRead(A_2_speed_N_S);
-  speed_W_E = analogRead(A_4_speed_W_E);
+  precipitation = map(analogRead(A_0_precipitation), 1023, 0, 100, 0);
+  speed_N_S = analogRead(A_1_speed_N_S);
+  speed_W_E = analogRead(A_2_speed_W_E);
   time = millis();
-  
+    for(int i=0; i < 1024; i++){
+    speed_N_S+=analogRead(A_1_speed_N_S);
+    speed_W_E+=analogRead(A_2_speed_W_E);
+  }
+  double Speed = 0.1 * sqrt(abs((double) speed_W_E - 32700.0)+abs(((double)speed_N_S-20500.0)*1.6));
   Serial.print(time);
   Serial.print(" ");
-  Serial.print(temp_c, DEC);
+  Serial.print(temp_c);
   Serial.print(" ");
-  Serial.print(pressure*0.01, DEC);
+  Serial.print(pressure*0.01;
   Serial.print(" ");
-  Serial.print(humidity, DEC);
+  Serial.print(humidity);
   Serial.print(" ");
-  Serial.print(fallout);
+  Serial.print(precipitation);
   Serial.print(" ");
   Serial.print(speed_N_S);
   Serial.print(" ");
   Serial.print(speed_W_E);
   Serial.print(" ");
+  Serial.print(Speed);
+  Serial.print(" ");
   Serial.print("0 ");
-  Serial.print("0");
+  Serial.print("0 ");
   Serial.println(";");
 
 }
